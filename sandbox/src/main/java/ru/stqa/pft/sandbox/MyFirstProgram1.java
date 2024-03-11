d447ee670fd00b5ae9e6b54cc9501a054538113e
olegmas19 <olegmas19@mail.ru>
2024-03-11 16:30:09 +0300

    Added a few functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram1.java
@@ -3,5 +3,15 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Hello("World");
+        double NotArea = area(2.0);
+    }
+
+    public static void Hello(String str){
+        System.out.println("Hello," +" "+ str + "!");
+    }
+
+    public static double area(double q){
+        return q * q;
     }
 }
