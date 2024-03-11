6fa03021eedec94149f8e0c48a50cf5e105cbea8
olegmas19 <olegmas19@mail.ru>
2024-03-11 15:18:04 +0300

    Created a package for my first program

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram1.java
@@ -1,4 +1,6 @@
-public class HelloWorld {
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
